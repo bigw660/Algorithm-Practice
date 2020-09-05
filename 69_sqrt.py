cd719c3a6673dd732630350d0a830f79c6f3cb9e
yihsung <61091334+yihsung@users.noreply.github.com>
2020-09-05 14:13:21 -0600

    Create 69_sqrt.py

---
+++ 69_sqrt.py
@@ -0,0 +1,23 @@
+def mySqrt(self, x):
+        # bin search
+        
+        # find the lower bound by log
+        y, b = x, 0
+        while y:
+            y = y >> 1
+            b += 1
+        
+        lo, hi = min(2**((b-1)//2), 0), x 
+        while lo < hi:
+            mid = (lo+hi) // 2 + 1
+            print(lo, hi, mid)
+            if mid*mid == x:
+                return mid
+            elif mid*mid < x:
+                lo = mid
+            else:
+                hi = mid - 1
+                
+        return lo
+
+# A better way is by Newton's method
