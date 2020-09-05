479f8f71293471a9b993278c8f965addee55aedc
yihsung <61091334+yihsung@users.noreply.github.com>
2020-09-05 12:33:35 -0600

    Update 378_Kth_Small_Elmement_In_Sorted_Array.py

--- 378_Kth_Small_Elmement_In_Sorted_Array.py
+++ 378_Kth_Small_Elmement_In_Sorted_Array.py
@@ -1,16 +1,16 @@
 def kthSmallest(self, matrix, k):
   n = len(matrix)
   lo, hi = matrix[0][0], matrix[-1][-1] while lo < hi:
-              mid = (lo+hi) // 2
+    mid = (lo+hi) // 2
   p, q, c = n-1, 0, 0 #(p,q) moving point, c: count # saddleback search
   while 0<=p and q < n:
-  if matrix[p][q] > mid: p -= 1
-                  else:
-                      c += p + 1 # update number of elm < mid; 1 is bec p being an ind
-                      q += 1
-              # binary search
-              if c < k:
-                  lo = mid + 1
-  else:
-  hi = mid
-  return lo
+    if matrix[p][q] > mid: p -= 1
+      else:
+        c += p + 1 # update number of elm < mid; 1 is bec p being an ind
+        q += 1
+        # binary search
+        if c < k:
+            lo = mid + 1 
+    else:
+      hi = mid
+      return lo
