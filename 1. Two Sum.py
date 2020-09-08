62b7502623177f392c33029301dfd354fee97a80
yihsung <61091334+yihsung@users.noreply.github.com>
2020-09-07 23:31:18 -0600

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,1 +1,10 @@
-
+def twoSum(self, nums, target):
+        # hash table
+        
+        seen = dict()
+        
+        for i, x in enumerate(nums):
+            if target - x in seen:
+                return([seen[target-x], i])
+            else:
+                seen[x] = i
